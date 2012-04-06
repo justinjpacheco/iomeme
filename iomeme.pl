e2caa762a2b0082853a691bd78619be6f46823d8
Justin Pacheco <justinjpacheco@gmail.com>
2012-04-06 06:15:21 -0400

    corrected text rendering and bottom height calculation

--- iomeme.pl
+++ iomeme.pl
@@ -5,16 +5,26 @@ use Data::Dumper;
 use Imager;
 use Imager::Font::Wrap;
 
+sub get_image_parameters {
+}
+
 my $image = Imager->new();
 $image->read(file => 'the-most-interesting-man-in-the-world.jpg');
 
-my $font_size = get_font_size(
-    get_text(),
+my ($font_size,$font_height) = get_font_size(
+    get_top_text(),
     $image->getwidth,
     ($image->getheight / 5)
 );
 
-render_text('bottom',$image,get_font(),get_text(),$font_size);
+render_text('top',$image,get_font(),get_top_text(),$font_size,$font_height);
+
+my ($font_size,$font_height) = get_font_size(
+    get_bottom_text(),
+    $image->getwidth,
+    ($image->getheight / 5)
+);
+render_text('bottom',$image,get_font(),get_bottom_text(),$font_size,$font_height);
 
 $image->write(file=>'output.png')
     or die 'Cannot save output.png: ', $image->errstr;
@@ -24,10 +34,19 @@ sub get_font {
         or die "Cannot load impact.ttf: ", Imager->errstr;
 }
 
-sub get_text {
+sub get_top_text {
 
 my $text = <<EOSTR;
-if you try to participate
+I don't always make memes
+EOSTR
+
+    return uc($text);
+}
+
+sub get_bottom_text {
+
+my $text = <<EOSTR;
+but when i do i make my own generator to do it
 EOSTR
 
     return uc($text);
@@ -37,13 +56,14 @@ sub get_font_size {
 
     my ($text,$width,$height) = @_;
 
-    my $adjusted_height = ($height / 5);
     my $text_size = 50;
     my $font = get_font();
 
     while (1) {
 
-        my (undef,undef,$b_width,$b_height) = Imager::Font::Wrap->wrap_text(
+        my $savepos;
+
+        my (undef,undef,$w,$h) = Imager::Font::Wrap->wrap_text(
             image  => undef,
             font   => $font,
             string => $text,
@@ -51,16 +71,19 @@ sub get_font_size {
             color => 'white',
             justify => 'center',
             width => $width,
-            height => $adjusted_height
+            height => $height,
+            savepos => \$savepos
         );
 
-        if ($b_height > $adjusted_height) {
+        printf("save pos: %d\n", $savepos);
+
+        if ($savepos < length($text)) {
 
             $text_size = $text_size - 1;
 
         } else {
 
-            return $text_size;
+            return ($text_size,$h);
 
         }
 
@@ -70,21 +93,23 @@ sub get_font_size {
 
 sub get_text_start_pos {
 
-    my ($type,$width,$height) = @_;
+    my ($type,$width,$height,$text_height) = @_;
 
     if ($type eq 'bottom') {
-        my $y_pos = ($height - ($height / 5) );
+        my $y_pos = ($height - $text_height) - 10;
         return(0,$y_pos);
     }
 
-    return (0,0);
+    return (15,0);
 }
 
 sub render_text {
 
-    my ($type,$image,$font,$text,$text_size) = @_;
+    my ($type,$image,$font,$text,$text_size,$text_height) = @_;
 
-    my ($x,$y) = get_text_start_pos($type,$image->getwidth,$image->getheight);
+    my ($x,$y) = get_text_start_pos(
+        $type,$image->getwidth,$image->getheight,$text_height
+    );
 
     my $pos = [
         [($x - 1),($y - 1)],
