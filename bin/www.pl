f600827d9b1739d31800c912ef37778afaf683cc
Justin Pacheco <justinjpacheco@gmail.com>
2012-04-08 11:47:34 -0400

    added mojolicious dev server

---
+++ bin/www.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/env perl
+
+use Mojolicious::Lite;
+
+use lib qw/lib/;
+use iomeme;
+
+get '/:type/:top/:bottom' => sub {
+    my $self = shift;
+
+    my $type = $self->param('type');
+    my $top = $self->param('top');
+    my $bottom = $self->param('bottom');
+
+    my $opt = {
+        meme => $type,
+        top => uc($top),
+        bottom => uc($bottom)
+    };
+
+    my $meme = iomeme->new(%$opt);
+
+    $self->render( data => $meme->render, format => $meme->format );
+};
+
+app->start;
