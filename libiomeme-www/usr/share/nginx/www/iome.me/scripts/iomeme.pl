972124c44b595d598e6cbc659c44fb46789cb1e3
Justin Pacheco <justinjpacheco@gmail.com>
2012-04-10 17:31:04 -0400

    added cdn redirection support. now supports all character symbols from url

--- libiomeme-www/usr/share/nginx/www/iome.me/scripts/iomeme.pl
+++ libiomeme-www/usr/share/nginx/www/iome.me/scripts/iomeme.pl
@@ -2,15 +2,65 @@
 use strict;
 
 use iomeme;
+use Mojo::URL;
+use Mojo::Util qw/url_unescape/;
 use Mojolicious::Lite;
+
+get '/cdn/*' => sub {
+    my $self = shift;
+
+    my $url_path = Mojo::Util::url_unescape($self->req->url);
+
+    # remove leading / from url path
+    #
+    $url_path =~ s/^\///;
+
+    my (undef,$type,$top,$bottom) = split('/',$url_path);
+
+    my $meme = get_meme($type,$top,$bottom);
+    $self->render( data => $meme->render, format => $meme->format );
+
+} => 'cdn';
 
 get '/*' => sub {
     my $self = shift;
 
-    # mojo considers "." to be a different url path
-    # so we'll get it out ourselves
+    if (1) {
+        my $cdn_url = get_cdn_url($self);
+        return $self->redirect_to($cdn_url);
+    }
+
+    my $url_path = Mojo::Util::url_unescape($self->req->url);
+
+    # remove leading / from url path
     #
-    my ($type,$top,$bottom) = @{$self->req->url->path->parts};
+    $url_path =~ s/^\///;
+
+    my ($type,$top,$bottom) = split('/',$url_path);
+
+    my $meme = get_meme($type,$top,$bottom);
+    $self->render( data => $meme->render, format => $meme->format );
+
+} => 'index';
+
+sub get_cdn_url {
+    my $self = shift;
+    my $req = $self->req->url;
+
+    my $base = $self->req->url->base->host;
+    my $cdn_domain = 'nyud.net';
+    my $cdn_host = $base . "." . $cdn_domain;
+
+    my $url = Mojo::URL->new;
+    $url->scheme('http');
+    $url->host($cdn_host);
+    $url->path($self->url_for('/cdn') . $self->req->url->to_string);
+
+    return $url;
+}
+
+sub get_meme {
+    my ($type,$top,$bottom) = @_;
 
     # limit the amount of text in top and bottom
     #
@@ -23,10 +73,8 @@ get '/*' => sub {
         bottom => uc($bottom or "")
     };
 
-    my $meme = iomeme->new(%$opt);
+    return iomeme->new(%$opt);
 
-    $self->render( data => $meme->render, format => $meme->format );
-
-};
+}
 
 app->start;
