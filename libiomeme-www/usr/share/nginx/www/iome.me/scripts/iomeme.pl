4047e4d2b970c9df0eec932c60a96006191a2ab3
Justin Pacheco <justinjpacheco@gmail.com>
2012-04-09 21:22:39 -0400

    improved placeholders. using param globbing to capture funny chars

--- libiomeme-www/usr/share/nginx/www/iome.me/scripts/iomeme.pl
+++ libiomeme-www/usr/share/nginx/www/iome.me/scripts/iomeme.pl
@@ -1,24 +1,32 @@
 #!/usr/bin/env perl
+use strict;
 
-use Mojolicious::Lite;
 use iomeme;
+use Mojolicious::Lite;
 
-get '/:type/:top/:bottom' => sub {
+get '/*' => sub {
     my $self = shift;
 
-    my $type = $self->param('type');
-    my $top = $self->param('top');
-    my $bottom = $self->param('bottom');
+    # mojo considers "." to be a different url path
+    # so we'll get it out ourselves
+    #
+    my ($type,$top,$bottom) = @{$self->req->url->path->parts};
+
+    # limit the amount of text in top and bottom
+    #
+    $top = substr($top,0,50) if ($top);
+    $bottom = substr($bottom,0,50) if ($bottom);
 
     my $opt = {
         meme => $type,
-        top => uc($top),
-        bottom => uc($bottom)
+        top => uc($top or ""),
+        bottom => uc($bottom or "")
     };
 
     my $meme = iomeme->new(%$opt);
 
     $self->render( data => $meme->render, format => $meme->format );
+
 };
 
 app->start;
