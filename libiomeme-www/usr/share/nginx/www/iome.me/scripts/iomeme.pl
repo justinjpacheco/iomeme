f16f8ee50e4445ab96da955c44c3fa28b48f1b63
Justin Pacheco <justinjpacheco@gmail.com>
2012-04-12 12:18:01 -0400

    removed cdn logic. updated nginx to send the request url in a special header. added file caching

--- libiomeme-www/usr/share/nginx/www/iome.me/scripts/iomeme.pl
+++ libiomeme-www/usr/share/nginx/www/iome.me/scripts/iomeme.pl
@@ -4,33 +4,21 @@ use strict;
 use iomeme;
 use Mojo::URL;
 use Mojo::Util qw/url_unescape/;
+use Cache::FastMmap;
 use Mojolicious::Lite;
 
-get '/cdn/*' => sub {
-    my $self = shift;
-
-    my $url_path = Mojo::Util::url_unescape($self->req->url);
-
-    # remove leading / from url path
-    #
-    $url_path =~ s/^\///;
-
-    my (undef,$type,$top,$bottom) = split('/',$url_path);
-
-    my $meme = get_meme($type,$top,$bottom);
-    $self->render( data => $meme->render, format => $meme->format );
-
-} => 'cdn';
+# Uses vaguely sane defaults
+my $cache = Cache::FastMmap->new();
 
 get '/*' => sub {
     my $self = shift;
 
-    if (1) {
-        my $cdn_url = get_cdn_url($self);
-        return $self->redirect_to($cdn_url);
-    }
-
     my $url_path = Mojo::Util::url_unescape($self->req->url);
+
+    use Data::Dumper;
+    $self->app->log->debug(Dumper $sef->req->env);
+    $self->app->log->debug(Dumper $self->req->content->headers->to_string);
+    #$self->app->log->debug(Dumper %ENV);
 
     # remove leading / from url path
     #
@@ -38,26 +26,25 @@ get '/*' => sub {
 
     my ($type,$top,$bottom) = split('/',$url_path);
 
-    my $meme = get_meme($type,$top,$bottom);
-    $self->render( data => $meme->render, format => $meme->format );
+    my $key = $type.$top.$bottom;
+
+    my ($image_data,$image_format) = undef;
+
+    if ($cache->get($key)) {
+
+        ($image_data,$image_format) = @{$cache->get($key)};
+
+    } else {
+
+        my $meme = get_meme($type,$top,$bottom);
+        $cache->set($key,[$meme->render,$meme->format]);
+        ($image_data,$image_format) = @{$cache->get($key)};
+
+    }
+
+    $self->render( data => $image_data, format => $image_format );
 
 } => 'index';
-
-sub get_cdn_url {
-    my $self = shift;
-    my $req = $self->req->url;
-
-    my $base = $self->req->url->base->host;
-    my $cdn_domain = 'nyud.net';
-    my $cdn_host = $base . "." . $cdn_domain;
-
-    my $url = Mojo::URL->new;
-    $url->scheme('http');
-    $url->host($cdn_host);
-    $url->path($self->url_for('/cdn') . $self->req->url->to_string);
-
-    return $url;
-}
 
 sub get_meme {
     my ($type,$top,$bottom) = @_;
